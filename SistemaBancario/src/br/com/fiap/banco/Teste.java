69baf34de6d47ee1c23e9db13c63384d17bda138
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-04-24 15:34:19 -0300

    Add files via upload

---
+++ SistemaBancario/src/br/com/fiap/banco/Teste.java
@@ -0,0 +1,21 @@
+package br.com.fiap.banco;
+
+public class Teste {
+	public static void main(String[] args) {
+
+		Conta cc = new Conta();
+		cc.agencia = 123;
+		cc.numero = 321;
+		cc.saldo = 50.0;
+		
+		cc.depositar(1000);
+		
+		System.out.println(cc.verificarSaldo());
+		
+		Conta poupanca = new Conta (111, 222, 1000);
+		poupanca.retirar(50);
+		
+		System.out.println(poupanca.verificarSaldo());
+		
+	}
+}
